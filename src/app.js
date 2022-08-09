36ae826ea5ec06ad5ca40da7ff5646bd094dc6e7
Covasc <lucas.rodmarques@gmail.com>
2022-08-08 22:28:16 -0300

    teste da API

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express, {json} from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import { getTest } from './controllers.js';
 
 const app = express();
 app.use(cors());
@@ -9,6 +10,7 @@ app.use(json());
 dotenv.config();
 const PORT = process.env.PORT;
 
+app.get('/test', getTest)
 
 app.listen(PORT, () => {
     console.log("server running")
