3c6a6e11f997fd2f3987bde63f1eb57f2e8dee54
emily <nakanodepaula@gmail.com>
2022-08-09 19:24:12 -0300

    chore: validate post schema middleware

--- src/routes/postsRouter.js
+++ src/routes/postsRouter.js
@@ -1,6 +1,9 @@
 import { Router } from "express";
 import { createPost } from "../controllers/postsController.js";
 import { validateToken } from "../middlewares/validadeTokenMiddleware.js";
+import { validateSchema } from "../middlewares/schemaValidatorMiddleware.js";
+import postSchema from "../schemas/postSchema.js";
+
 const postsRouter = Router();
-postsRouter.post('/posts', validateToken, createPost);
+postsRouter.post('/posts', validateToken, validateSchema(postSchema), createPost);
 export default postsRouter;
