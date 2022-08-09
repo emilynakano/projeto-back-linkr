53a464c02c88e02a71ad91612c8dcccd384855d1
Dayane <dayanecarolina.oliveira@gmail.com>
2022-08-09 16:44:05 -0300

    teste

--- src/config/db.js
+++ src/config/db.js
@@ -1,16 +1,16 @@
 import pg from 'pg';
-import dotenv from 'dotenv';
-
+import dotenv from "dotenv";
 dotenv.config();
 
 const { Pool } = pg;
 
-const db = new Pool({
-    host: 'localhost',
-    port: 5432,
-    user: 'postgres',
-    password: 'postgres1',
-    database: 'linkr'
-});
+const databaseConfig = {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+}
+
+const db = new Pool(databaseConfig);
 
 export default db;
