43fe8b596d39b59e3fd796cb51022afaf0648643
Covasc <lucas.rodmarques@gmail.com>
2022-08-08 22:24:09 -0300

    add db.js

--- src/app.js
+++ src/app.js
@@ -2,11 +2,14 @@ import express, {json} from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 
-dotenv.config();
-
 const app = express();
-
 app.use(cors());
 app.use(json());
 
-app.listen(process.env.PORT)
+dotenv.config();
+const PORT = process.env.PORT;
+
+
+app.listen(PORT, () => {
+    console.log("server running")
+});
