a5b40a18bf427715a5617c92b54ce5c0f10b5621
Danilo <dandam3006@gmail.com>
2022-08-10 16:58:29 -0300

    feat: add route to get user posts by id

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -1,4 +1,29 @@
+import { getPostsByPosterId } from "../repositories/postsRepository.js";
 
 export function createPost(req, res) {
     res.send("oi")
+}
+
+export async function getUserPostsById(req, res) {
+
+    const { id } = req.params;
+
+    if (!id || isNaN(Number(id))) {
+        res.status(400).send("Invalid id!");
+        return;
+    }
+
+    try {
+        
+        const { rows: posts } = await getPostsByPosterId(id);
+        res.status(200).send(posts);
+        return;
+
+    } catch (error) {
+        console.log(chalk.bold.red("Erro no servidor!"));
+        res.status(500).send({
+          message: "Internal server error while getting posts!",
+        });
+        return;
+    }
 }
--- src/repositories/postsRepository.js
+++ src/repositories/postsRepository.js
@@ -3,3 +3,8 @@ export async function insertPost(url, content, posterId) {
     return db.query(
         `INSERT INTO posts ("posterId", url, content) VALUES ($1, $2, $3)`, [posterId, url, content])
 }
+
+export async function getPostsByPosterId(posterId) {
+    return db.query(
+        `SELECT *, users.name, users."profilePicture" FROM posts JOIN users ON "posterId" = users.id WHERE "posterId" = $1`, [posterId])
+}
