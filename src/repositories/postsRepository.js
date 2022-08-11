2beee4ef90c602c2b2d20144d22b736aa5a1f7c6
Dayane <dayanecarolina.oliveira@gmail.com>
2022-08-11 10:54:53 -0300

    feat: edit post

--- src/repositories/postsRepository.js
+++ src/repositories/postsRepository.js
@@ -13,3 +13,18 @@ export async function getAllPosts() {
     return db.query(`SELECT users.name, posts.content, users."profilePicture", posts.url FROM users JOIN posts ON users.id=posts."posterId" ORDER BY posts."createdAt" DESC LIMIT 20;
     `)
 }
+
+export async function findPostById(id){
+    return db.query(
+        `SELECT * FROM posts
+        WHERE id = $1`,[id]
+    );
+}
+
+export async function updateDescription(id,description){
+    return db.query(
+        `UPDATE posts
+        SET description = $1
+        WHERE id = $2`,[description,id]
+    );
+}
