66ff0ee95bd7cdc9cdec23196645f3c354bc9511
emily <nakanodepaula@gmail.com>
2022-08-11 17:43:51 -0300

    fix: reestructure insert like

--- src/controllers/likesController.js
+++ src/controllers/likesController.js
@@ -1,7 +1,11 @@
 import { insertLike } from "../repositories/likesRepository.js";
 export async function createLike(req, res) {
-    const postId = req.body.id;
-    const userId = res.locals.user.id;
-    await insertLike(userId, postId)
-    res.send("userId")
+    try {
+        const postId = req.body.id;
+        const userId = res.locals.user.id;
+        await insertLike(userId, postId)
+        res.send(200)
+    } catch {
+        res.sendStatus(500)
+    }
 }
