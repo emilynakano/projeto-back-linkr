315fc2d37ff0d69373b4b29a0522d3d088f51aca
emily <nakanodepaula@gmail.com>
2022-08-11 22:55:41 -0300

    feat: get al likes

--- src/controllers/likesController.js
+++ src/controllers/likesController.js
@@ -1,11 +1,20 @@
-import { insertLike } from "../repositories/likesRepository.js";
+import { insertLike, getPosts, getLikes } from "../repositories/likesRepository.js";
 export async function createLike(req, res) {
     try {
         const postId = req.body.id;
         const userId = res.locals.user.id;
         await insertLike(userId, postId)
-        res.send(200)
+        res.sendStatus(201)
     } catch {
         res.sendStatus(500)
     }
+}
+export async function getAllLikes (req, res) {
+    const {rows: posts} = await getPosts();
+    let allLikes = [];
+    for(const post of posts) {
+        const {rows: likes} = await getLikes(post);
+        allLikes.push({id:post.id, users: likes})
+    }
+    res.send(allLikes)
 }
--- src/repositories/likesRepository.js
+++ src/repositories/likesRepository.js
@@ -4,3 +4,17 @@ export async function insertLike (userId, postId){
         `INSERT INTO likes ("userId", "postId") VALUES ($1, $2)`, [userId, postId]
         )
 }
+export async function getUserLike (userId) {
+    return db.query (
+        `SELECT "postId" AS id FROM likes WHERE "userId"=$1`, [userId]
+    )
+}
+export async function getPosts () {
+    return db.query (
+        `SELECT * FROM posts ORDER BY "createdAt" DESC LIMIT 20`
+    )
+}
+export async function getLikes(post) {
+    return db.query(`SELECT users.name FROM likes JOIN users ON users."id"=likes."userId" WHERE "postId"=$1`, [post.id]
+    );
+}
