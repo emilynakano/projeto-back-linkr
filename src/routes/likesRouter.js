00c23332daf6551a2d1fd654f73e4a36cb1d4d3c
emily <nakanodepaula@gmail.com>
2022-08-11 22:55:41 -0300

    feat: get al likes

--- src/routes/likesRouter.js
+++ src/routes/likesRouter.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
 
-import { createLike } from "../controllers/likesController.js";
+import { createLike, getAllLikes } from "../controllers/likesController.js";
 import { validateToken } from "../middlewares/validadeTokenMiddleware.js";
 
 const likesRouter = Router();
-likesRouter.post("/likes", validateToken, createLike)
+likesRouter.post("/likes", validateToken, createLike);
+likesRouter.get("/likes", validateToken, getAllLikes)
 export default likesRouter
