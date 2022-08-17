16df401ba4559c0ead373742f3a8fab38e66dd56
Dayane <dayanecarolina.oliveira@gmail.com>
2022-08-16 23:21:43 -0300

    feat: follow and unfollow user

--- src/config/db.js
+++ src/config/db.js
@@ -7,7 +7,7 @@ const { Pool } = pg;
 //process.env.DATABASE_URL
 
 const databaseConfig = {
-    connectionString: `postgres://usrgatzhrqowbr:b3c13f7d34566a972d9ef77205f203ab56aac43f4db53fbba77064030c17bc1b@ec2-54-85-56-210.compute-1.amazonaws.com:5432/dep8ohlqan5m68`,
+    connectionString: `postgres://postgres:Siga123@@localhost:5432/linkr`,
     ssl: {
         rejectUnauthorized: false
     }
---
+++ src/controllers/followController.js
@@ -0,0 +1,38 @@
+import chalk from "chalk";
+import { getFollow, insertFollow, unfollow } from "../repositories/followRepository.js";
+
+export async function followUser(req,res){
+
+    const { id:followedUserId } = req.params;
+    const {id:followerUserId} = res.locals.user;
+    
+    if (!followedUserId  || isNaN(Number(followedUserId))) {
+        res.status(400).send("Invalid id!");
+        return;
+    }
+
+    if (parseInt(followerUserId)===parseInt(followedUserId)){
+        res.status(401).send("User follower and user followed are the same user!");
+        return;
+    }
+
+    try {
+        const follow = await getFollow(followerUserId,followedUserId);
+        
+        if(follow.rowCount===0){
+            await insertFollow(followerUserId,followedUserId);
+            res.status(200).send({isfollow:true});
+            return;
+        }else{
+            await unfollow(followerUserId,followedUserId);
+            res.status(200).send({isfollow:false});
+            return;
+        }
+    } catch (error) {
+        console.log(chalk.bold.red("Erro no servidor!"));
+        res.status(500).send({
+          message: "Internal server error follow user!",
+        });
+        return;   
+    }
+}
