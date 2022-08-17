a594e6f41574dfd572fc5d8e33e4d963c8a25b7f
emily <nakanodepaula@gmail.com>
2022-08-16 22:14:49 -0300

    fix: add res in create comment

--- src/routes/commentsRouter.js
+++ src/routes/commentsRouter.js
@@ -3,5 +3,5 @@ import { getComments, newComment } from "../controllers/commentsController.js";
 import { validateToken } from "../middlewares/validadeTokenMiddleware.js"
 const commentRouter = Router();
 commentRouter.get("/comments", getComments);
-commentRouter.post("/comments", validateToken ,newComment)
+commentRouter.post("/comments", validateToken, newComment)
 export default commentRouter
