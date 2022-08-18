ec86dc9c3036b2dc15160dc30558ee25b2b99ecd
Dayane <dayanecarolina.oliveira@gmail.com>
2022-08-17 21:04:17 -0300

    test: remove validate token

--- src/routes/followRouter.js
+++ src/routes/followRouter.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import { validateToken } from "../middlewares/validadeTokenMiddleware.js";
+// import { validateToken } from "../middlewares/validadeTokenMiddleware.js";
 import { followUser } from "../controllers/followController.js";
 
 const followRouter = Router();
 
-followRouter.post("/user/:id/follow",validateToken, followUser);
+followRouter.post("/user/:id/follow", followUser);
 
 export default followRouter;
