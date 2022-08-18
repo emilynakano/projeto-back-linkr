d7113081e777080370c78b5872fc63a361ab852a
Dayane <dayanecarolina.oliveira@gmail.com>
2022-08-17 21:12:45 -0300

    fix: follow router

--- src/routes/followRouter.js
+++ src/routes/followRouter.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
-// import { validateToken } from "../middlewares/validadeTokenMiddleware.js";
+import { validateToken } from "../middlewares/validadeTokenMiddleware.js";
 import { followUser } from "../controllers/followController.js";
 
 const followRouter = Router();
 
-followRouter.post("/user/:followedUserId/follow", followUser);
+followRouter.post("/user/:followedUserId/follow", validateToken, followUser);
 
 export default followRouter;
