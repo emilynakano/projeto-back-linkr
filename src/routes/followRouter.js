ab57ece9010124387f55dd95a3d5c126a6558b5e
emily <nakanodepaula@gmail.com>
2022-08-18 14:34:51 -0300

    feat: get user follows

--- src/routes/followRouter.js
+++ src/routes/followRouter.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { validateToken } from "../middlewares/validadeTokenMiddleware.js";
-import { followUser, statusFollow } from "../controllers/followController.js";
+import { followUser, getFollowsUser, statusFollow } from "../controllers/followController.js";
 
 const followRouter = Router();
 
 followRouter.post("/user/:id/follow",validateToken, followUser);
 followRouter.get("/user/:id/follow",validateToken, statusFollow);
+followRouter.get("/follow/user", validateToken, getFollowsUser)
 
 export default followRouter;
