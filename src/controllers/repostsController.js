6f3a22d7f3ede428bc856ad5e98c4b0635c17e70
emily <nakanodepaula@gmail.com>
2022-08-19 11:31:00 -0300

    feat: create a repost

--- src/controllers/repostsController.js
+++ src/controllers/repostsController.js
@@ -1,17 +1,21 @@
 import { postExists, postRepost } from "../repositories/repostsRepository.js";
 
 export async function newRepost (req, res) {
-    const userId = res.locals.user.id;
+    const reposterName = res.locals.user.name;
     const postId = req.body.id;
     try {
 
-        const post = await postExists(postId);
+        const {rowCount, rows:post} = await postExists(postId);
 
-        if(post.rowCount === 0) {
+        if(rowCount === 0) {
             return res.status(404).send("post id not found");
         }
 
-        await postRepost(postId, userId);
+        const repostUrl = post[0].url 
+        const repostContent = post[0].content;
+        const posterId = post[0].posterId;
+        
+        await postRepost(postId, posterId, repostUrl, repostContent, reposterName);
 
         res.sendStatus(201)
     } catch (error) {
